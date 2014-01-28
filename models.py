65c0fa6b47a491f7e625408b477a6088d482c7d3
Fabio Fleitas <cubanfabio@gmail.com>
2014-01-28 16:42:24 -0500

    Setup datastore models

---
+++ models.py
@@ -0,0 +1,55 @@
+from google.appengine.ext import db
+
+class Company(db.Model):
+    company_key = db.StringProperty()
+    username = db.StringProperty()
+    password = db.StringProperty()
+
+    name = db.StringProperty()
+    is_buyer = db.BooleanProperty()
+    is_supplier = db.BooleanProperty()
+    address = db.PostalAddressProperty()
+    tax_id = db.StringProperty()
+    phone = db.PhoneNumberProperty()
+    email = db.EmailProperty()
+
+    contact_person = db.StringProperty()
+    contact_phone = db.PhoneNumberProperty()
+
+    suppliers = db.StringListProperty()
+    buyers = db.StringListProperty()
+
+    cash_committed = db.FloatProperty()
+    apr = db.FloatProperty()
+
+class Invoice(db.Model):
+    inv_key = db.StringProperty()
+    buyer_key = db.StringProperty()
+    supplier_key = db.StringProperty()
+
+    buyer_inv_key = db.StringProperty()
+    supplier_inv_key = db.StringProperty()
+
+    amount = db.FloatProperty()
+
+    inv_date = db.DateTimeProperty()
+    due_date = db.DateTimeProperty()
+
+    po_num = db.StringProperty()
+    description = db.TextProperty()
+
+    date_approved = db.DateTimeProperty()
+    date_uploaded = db.DateTimeProperty()
+
+class Bid(db.Model):
+    bid_key = db.StringProperty()
+    inv_key = db.StringProperty()
+
+    discount = db.FloatProperty()
+
+    days_acc = db.IntegerProperty()
+    date_due = db.DateTimeProperty() # date need cash by
+    amount = db.FloatProperty()
+
+    is_standing_offer = db.BooleanProperty()
+    date_submit = db.DateTimeProperty()
