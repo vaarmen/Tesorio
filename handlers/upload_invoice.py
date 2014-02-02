8f950c4688bf2112a35a3bf906342183b7a8827a
Fabio Fleitas <cubanfabio@gmail.com>
2014-02-02 18:23:17 -0500

    Changed variable: supplier_inv_key -> supplier_inv_number

--- handlers/upload_invoice.py
+++ handlers/upload_invoice.py
@@ -11,7 +11,7 @@ class UploadInvoiceHandler(Handler):
     def post(self):
         # Request form input
         buyer_inv_key = self.request.get("binvoice-key")
-        supplier_inv_key = self.request.get("sinvoice-key")
+        supplier_inv_number = self.request.get("sinvoice-number")
         amount = self.request.get("amount")
         inv_date = self.request.get("invoice-date")
         due_date = self.request.get("due-date")
@@ -22,7 +22,7 @@ class UploadInvoiceHandler(Handler):
         # Log all form input
         logging.info("UploadInvoiceHandler POST Method Logs")
         logging.info("Buyer Invoice Key: " + buyer_inv_key)
-        logging.info("Supplier Invoice Key: " + supplier_inv_key)
+        logging.info("Supplier Invoice Number: " + supplier_inv_number)
         logging.info("Amount: " + amount)
         logging.info("Invoice Date: " + inv_date)
         logging.info("Due Date: " + due_date)
@@ -33,7 +33,7 @@ class UploadInvoiceHandler(Handler):
         # Put form input into model and database
         invoice = models.Invoice()
         invoice.buyer_inv_key = buyer_inv_key
-        invoice.supplier_inv_key = supplier_inv_key
+        invoice.supplier_inv_number = supplier_inv_number
         invoice.amount = float(amount)
         invoice.inv_date = datetime.strptime(inv_date, '%m/%d/%Y')
         invoice.due_date = datetime.strptime(due_date, '%m/%d/%Y')
