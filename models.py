bd9fb684a6bade3e4771dc8402af45c47ff56143
Fabio Fleitas <cubanfabio@gmail.com>
2014-02-08 16:40:10 -0500

    Updated models

--- models.py
+++ models.py
@@ -23,35 +23,42 @@ class Company(ndb.Model):
     cash_committed = ndb.FloatProperty()
     apr = ndb.FloatProperty()
 
-    date_submit = ndb.DateTimeProperty(auto_now_add=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
 
-class Invoice( ndb.Model):
+class Bid(ndb.Model):
+    inv_id = ndb.StringProperty()
+
+    discount = ndb.FloatProperty()
+
+    days_acc = ndb.IntegerProperty()
+    date_due = ndb.DateProperty()
+    amount = ndb.FloatProperty()
+
+    status = ndb.StringProperty(choices=["Accepted", "Pending", "Declined"])
+    profit = ndb.FloatProperty()
+    apr = ndb.FloatProperty()
+
+    is_standing_offer = ndb.BooleanProperty()
+
+    created = ndb.DateTimeProperty(auto_now_add=True)
+
+class Invoice(ndb.Model):
     buyer_id = ndb.StringProperty()
     supplier_id = ndb.StringProperty()
 
     buyer_inv_key = ndb.StringProperty()
     supplier_inv_number = ndb.StringProperty()
 
+    recent_bid = ndb.KeyProperty()
+    status = ndb.StringProperty(choices=["Accepted", "Pending", "Expired", "Open", "Open BD"])
+
     amount = ndb.FloatProperty()
 
-    inv_date = ndb.DateTimeProperty()
-    due_date = ndb.DateTimeProperty()
+    inv_date = ndb.DateProperty()
+    due_date = ndb.DateProperty()
 
     po_num = ndb.StringProperty()
     description = ndb.TextProperty()
 
-    date_approved = ndb.DateTimeProperty()
-    date_submit = ndb.DateTimeProperty(auto_now_add=True)
-
-class Bid( ndb.Model):
-    inv_id = ndb.StringProperty()
-
-    discount = ndb.FloatProperty()
-
-    days_acc = ndb.IntegerProperty()
-    date_due = ndb.DateTimeProperty()
-    amount = ndb.FloatProperty()
-
-    is_standing_offer = ndb.BooleanProperty()
-
-    date_submit = ndb.DateTimeProperty(auto_now_add=True)
+    date_approved = ndb.DateProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
