632060b59571367f92bf029881985c91d4e78d41
Fabio Fleitas <cubanfabio@gmail.com>
2014-03-22 13:13:49 -0400

    working index page

--- app/views.py
+++ app/views.py
@@ -1,1 +1,54 @@
 # Create your views here.
+from django.views.generic import TemplateView
+
+class BaseView(TemplateView):
+    def render(self, **kwargs):
+        return self.render_to_response(self.get_context_data(**kwargs))
+
+    def get_context_data(self, **kwargs):
+        context = super(BaseView, self).get_context_data(**kwargs)
+        return context
+
+class IndexView(BaseView):
+    template_name = "index.jinja"
+
+    def get(self, request, *args, **kwargs):
+        return self.render()
+
+# class IndexView(TemplateView):
+#     template_name = "index.jinja"
+
+#     def get(self, request, *args, **kwargs):
+#         return self.render_to_response(self.get_context_data(lol="what is this"))
+
+#     def get_context_data(self, **kwargs):
+
+#         context = super(IndexView, self).get_context_data(**kwargs)
+#         # context['first_names'] = ['Nathan', 'Richard']
+
+#         return context
+
+
+# class SearchPageView(FormView):
+#     template_name = 'search/search.html'
+
+#     def get(self, request, *args, **kwargs):
+#         self.bookmarks = []
+#         self.show_results = False
+#         form = SearchForm(self.request.GET or None)
+#         if form.is_valid():
+#             self.show_results = True
+#             self.bookmarks = Bookmark.objects.filter(title__icontains=form.cleaned_data['query'])[:10]
+
+#         return self.render_to_response(self.get_context_data(form=form))
+
+
+#     def get_context_data(self, **kwargs):
+#         context = super(SearchPageView, self).get_context_data(**kwargs)
+#         context.update({
+#             'show_tags': True,
+#             'show_user': True,
+#             'show_results': self.show_results,
+#             'bookmarks': self.bookmarks
+#         })
+#         return context
--- tesorio/urls.py
+++ tesorio/urls.py
@@ -4,10 +4,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# import views
+from app import views
+
+# from django.views.generic import TemplateView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tesorio.views.home', name='home'),
     # url(r'^tesorio/', include('tesorio.foo.urls')),
+
+    url(r'^$', views.IndexView.as_view(), name='home'),
 
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
