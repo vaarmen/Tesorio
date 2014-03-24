0bae6f1426df5a105d98ecc1877ecf8807819070
Alex Rattray (Lil Mav) <alex@emeraldexam.com>
2014-03-23 20:35:16 -0400

    removed unnecessary libs

--- tesorio/management/commands/deploy.py
+++ tesorio/management/commands/deploy.py
@@ -2,33 +2,35 @@
 #
 # Author: Alex Rattray (rattray.alex@gmail.com)
 import subprocess
-
-print 'This is a script to deploy to gae'
-print 'intended usage: ./manage.py deploy'
-
+from django.core.management.base import BaseCommand, CommandError
 
 def shell(cmd, *args, **kwargs):
   return subprocess.call(cmd, shell=True, *args, **kwargs)
 
+class Command(BaseCommand):
+    # args = '<poll_id poll_id ...>'
+    help = 'This is a script to deploy to gae. Intended usage: ./manage.py deploy'
 
-shell('git commit --allow-empty -m "Deploying to Production at `date`"')
+    def handle(self, *args, **options):
 
-appcfg_update_cmd = 'appcfg.py update . --oauth2'
-print '-> About to run', appcfg_update_cmd
-shell(appcfg_update_cmd)
+        shell('git commit --allow-empty -m "Deploying to Production at `date`"')
 
-print '-> About to migrate prod'
-try:
-    version_id = '2'
-    shell('''
-        export SERVER_SOFTWARE='Production';
-        export CURRENT_VERSION_ID='{}.local'
-        python manage.py syncdb
-        python manage.py migrate
-    '''.format(version_id)
-    )
-finally:
-    shell('''
-        export CURRENT_VERSION_ID=''
-        export SERVER_SOFTWARE='Development'
-    ''')
+        appcfg_update_cmd = 'appcfg.py update . --oauth2'
+        self.stdout.write('-> About to run' + appcfg_update_cmd)
+        shell(appcfg_update_cmd)
+
+        self.stdout.write('-> About to migrate prod')
+        try:
+            version_id = '2'
+            shell('''
+                export SERVER_SOFTWARE='Production';
+                export CURRENT_VERSION_ID='{}.local';
+                python manage.py syncdb
+                python manage.py migrate
+            '''.format(version_id)
+            )
+        finally:
+            shell('''
+                export CURRENT_VERSION_ID=''
+                export SERVER_SOFTWARE='Development'
+            ''')
