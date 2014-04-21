7b13e297b3d58ff520e7b1bee1b4517162b293e6
Fabio Fleitas <cubanfabio@gmail.com>
2014-04-21 03:25:57 -0400

    changed /debug/ -> /__debug__/

--- tesorio/urls.py
+++ tesorio/urls.py
@@ -88,5 +88,5 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += patterns('',
-        url(r'^debug/', include(debug_toolbar.urls)),
+        url(r'^__debug__/', include(debug_toolbar.urls)),
     )
